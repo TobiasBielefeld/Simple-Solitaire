5f3f8c53f14f59d58f754e70fe220944606a0c37
Tobias <tobias.bielefeld@gmail.com>
2017-10-04 12:47:33 +0200

    new appearance for changelog

--- app/src/main/java/de/tobiasbielefeld/solitaire/ui/about/ChangeLogFragment.java
+++ app/src/main/java/de/tobiasbielefeld/solitaire/ui/about/ChangeLogFragment.java
@@ -18,12 +18,17 @@
 
 package de.tobiasbielefeld.solitaire.ui.about;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.IntegerRes;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import de.tobiasbielefeld.solitaire.R;
 
@@ -38,9 +43,82 @@ public class ChangeLogFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_tab3, container, false);
 
-        WebView webView = (WebView) view.findViewById(R.id.about_tab3_webview);
-        webView.loadUrl(getString(R.string.changelog_URL));
+        LinearLayout layoutContainer = (LinearLayout) view.findViewById(R.id.changelog_container);
+
+        String[] titles = new String[]{"3.9","3.8.6", "3.8.5", "3.8.4", "3.8.3", "3.8.2", "3.8.1",
+                "3.8", "3.7.2", "3.7.1", "3.7", "3.6.2", "3.6.1", "3.6", "3.5", "3.4", "3.3.5",
+                "3.3.4", "3.3.3", "3.3.2", "3.3.1", "3.3", "3.2", "3.1.5", "3.1.4", "3.1.3", "3.1.2",
+                "3.1.1", "3.1", "3.0.1", "3.0", "2.0.2", "2.0.1", "2.0", "1.4", "1.3", "1.2", "1.1",
+                "1.0"};
+
+
+        for (int i=0;i<titles.length;i++) {
+            CardView card = createCard();
+            layoutContainer.addView(card);
+
+            LinearLayout linearLayout = (LinearLayout) card.getChildAt(0);
+
+            linearLayout.addView(createTitle(titles[i]));
+            linearLayout.addView(createSeparator());
+            linearLayout.addView(createText(titles.length -i));
+        }
 
         return view;
+    }
+
+    private View createSeparator(){
+        View view = new View(getContext());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1);
+        view.setLayoutParams(params);
+        view.setBackgroundColor(getResources().getColor(R.color.colorDrawerSelected));
+
+        return view;
+    }
+
+    private TextView createText(int pos){
+        TextView view = new TextView(getContext());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        int padding = (int) (getResources().getDimension(R.dimen.changelog_text_padding));
+        view.setPadding(padding,padding,padding,padding);
+        view.setLayoutParams(params);
+        view.setText(getString(getResources().getIdentifier("changelog_" + Integer.toString(pos), "string", getActivity().getPackageName())));
+
+        return view;
+    }
+
+    private TextView createTitle(String text){
+        TextView view = new TextView(getContext());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        int padding = (int) (getResources().getDimension(R.dimen.changelog_title_padding));
+        view.setPadding(padding,padding,padding,padding);
+        view.setLayoutParams(params);
+        view.setTextAppearance(getContext(), android.R.style.TextAppearance_Medium);
+        view.setTypeface(view.getTypeface(), Typeface.BOLD);
+        view.setText("Ver. " + text);
+
+        return view;
+    }
+
+    private CardView createCard(){
+        CardView card = new CardView(getContext());
+
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        float radius = getResources().getDimension(R.dimen.changelog_card_corner_radius);
+        card.setLayoutParams(params);
+        card.setCardBackgroundColor(getResources().getColor(R.color.white));
+        card.setRadius(radius);
+        card.setUseCompatPadding(true);
+
+        LinearLayout layout = new LinearLayout(getContext());
+        ViewGroup.LayoutParams params2 = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.setLayoutParams(params2);
+
+        int padding = (int) (getResources().getDimension(R.dimen.changelog_linear_layout_Padding));
+        layout.setPadding(padding,padding,padding,padding);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        card.addView(layout);
+
+        return card;
     }
 }
