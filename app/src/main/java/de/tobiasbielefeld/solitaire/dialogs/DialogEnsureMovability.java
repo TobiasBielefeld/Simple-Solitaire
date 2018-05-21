774c678de5655f6377aed31509214948df3ca457
Tobias <tobias.bielefeld@gmail.com>
2018-05-21 20:02:37 +0200

    removing imports

--- app/src/main/java/de/tobiasbielefeld/solitaire/dialogs/DialogEnsureMovability.java
+++ app/src/main/java/de/tobiasbielefeld/solitaire/dialogs/DialogEnsureMovability.java
@@ -3,30 +3,18 @@ package de.tobiasbielefeld.solitaire.dialogs;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.concurrent.ExecutionException;
-
 import de.tobiasbielefeld.solitaire.R;
 import de.tobiasbielefeld.solitaire.classes.CustomDialogFragment;
-import de.tobiasbielefeld.solitaire.handler.HandlerLoadGame;
 import de.tobiasbielefeld.solitaire.helper.EnsureMovability;
-import de.tobiasbielefeld.solitaire.ui.GameManager;
-import de.tobiasbielefeld.solitaire.ui.statistics.StatisticsActivity;
 
-import static de.tobiasbielefeld.solitaire.SharedData.cards;
-import static de.tobiasbielefeld.solitaire.SharedData.currentGame;
 import static de.tobiasbielefeld.solitaire.SharedData.gameLogic;
-import static de.tobiasbielefeld.solitaire.SharedData.stacks;
 import static de.tobiasbielefeld.solitaire.SharedData.stopMovements;
-import static de.tobiasbielefeld.solitaire.classes.Card.movements.NONE;
 
 /**
  * Dialog to show while the EnsureMovability asyncTask is running. It shows a spinning wheel
